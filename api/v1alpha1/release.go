299e02ae4bb2603efe496f7121d6fd5f9d527aa2
Alex <1353716+alexstojda@users.noreply.github.com>
2023-08-02 13:05:27 -0400

    fix(DEVOPS-1803): Add JSON struct tags to api resources

--- api/v1alpha1/release.go
+++ api/v1alpha1/release.go
@@ -10,59 +10,59 @@ const ReleaseKind = "Release"
 
 type ReleaseMetadata struct {
 	// Name is the name of the release.
-	Name string `yaml:"name,omitempty"`
+	Name string `yaml:"name,omitempty" json:"name,omitempty"`
 }
 
 type ReleaseChart struct {
 	// Name is the name of the chart.
-	Name string `yaml:"name,omitempty"`
+	Name string `yaml:"name,omitempty" json:"name,omitempty"`
 
 	// RepoUrl is the url of the chart repository.
-	RepoUrl string `yaml:"repoUrl,omitempty"`
+	RepoUrl string `yaml:"repoUrl,omitempty" json:"repoUrl,omitempty"`
 
 	// Version of the chart.
-	Version string `yaml:"version,omitempty"`
+	Version string `yaml:"version,omitempty" json:"version,omitempty"`
 }
 
 type ReleaseSpec struct {
 	// Project is the name of the project that the release belongs to.
-	Project string `yaml:"project,omitempty"`
+	Project string `yaml:"project,omitempty" json:"project,omitempty"`
 
 	// Version of the release, typically corresponding to the image build version being deployed.
-	Version string `yaml:"version,omitempty"`
+	Version string `yaml:"version,omitempty" json:"version,omitempty"`
 
 	// Chart is the chart that the release is based on.
-	Chart ReleaseChart `yaml:"chart,omitempty"`
+	Chart ReleaseChart `yaml:"chart,omitempty" json:"chart,omitempty"`
 
 	// Values is the values to use to render the chart.
-	Values map[string]interface{} `yaml:"values,omitempty"`
+	Values map[string]interface{} `yaml:"values,omitempty" json:"values,omitempty"`
 }
 
 type Release struct {
 	// ApiVersion is the API version of the release.
-	ApiVersion string `yaml:"apiVersion,omitempty"`
+	ApiVersion string `yaml:"apiVersion,omitempty" json:"apiVersion,omitempty"`
 
 	// Kind is the kind of the release.
-	Kind string `yaml:"kind,omitempty"`
+	Kind string `yaml:"kind,omitempty" json:"kind,omitempty"`
 
 	// ReleaseMetadata is the metadata of the release.
-	ReleaseMetadata `yaml:"metadata,omitempty"`
+	ReleaseMetadata `yaml:"metadata,omitempty" json:"metadata,omitempty"`
 
 	// Spec is the spec of the release.
-	Spec ReleaseSpec `yaml:"spec,omitempty"`
+	Spec ReleaseSpec `yaml:"spec,omitempty" json:"spec,omitempty"`
 
 	// File represents the in-memory yaml file of the release.
-	File *yml.File `yaml:"-"`
+	File *yml.File `yaml:"-" json:"-"`
 
 	// Missing indicates whether the release is missing in the target environment. During a promotion,
 	// this allows to know whether the release will be created or updated.
-	Missing bool `yaml:"-"`
+	Missing bool `yaml:"-" json:"-"`
 
 	// Project is the project that the release belongs to.
-	Project *Project `yaml:"-"`
+	Project *Project `yaml:"-" json:"-"`
 
 	// Environment is the environment that the release is deployed to.
-	Environment *Environment `yaml:"-"`
+	Environment *Environment `yaml:"-" json:"-"`
 }
 
 func IsValidRelease(apiVersion, kind string) bool {
