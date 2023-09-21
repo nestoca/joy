1d79e9f6ff90b4138be7d3b1f0b0b41ff95ce9fe
Mathieu Frenette <silphid@users.noreply.github.com>
2023-09-21 13:15:35 -0400

    feat: add check for presence of gh CLI

--- internal/gh/gh.go
+++ internal/gh/gh.go
@@ -1,6 +1,7 @@
 package gh
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -9,13 +10,28 @@ import (
 
 // CreatePullRequest creates a pull request.
 func CreatePullRequest(args ...string) error {
+	err := ensureGHInstalled()
+	if err != nil {
+		return err
+	}
+
 	cmd := exec.Command("gh", args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		return fmt.Errorf("running gh command with args %q: %w", strings.Join(args, " "), err)
+	}
+	return nil
+}
+
+func ensureGHInstalled() error {
+	cmd := exec.Command("which", "gh")
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("🤓 This command requires the gh cli.\nSee: https://github.com/cli/cli")
+		return errors.New("missing gh cli dependency")
 	}
 	return nil
 }
