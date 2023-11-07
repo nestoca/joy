7869d5eb6b970c0bded32f47ca44fb565d804b34
David Desmarais-Michaud <david.desmarais-michaud@nesto.ca>
2023-11-07 12:09:56 -0500

    feat: IsLocked considers !lock tag

--- internal/yml/locking.go
+++ internal/yml/locking.go
@@ -8,10 +8,15 @@ import (
 
 var lockMarkerRegex = regexp.MustCompile(`(?i)(?m)^#+\s*lock\s*$`)
 
-func IsLocked(keyNode, valueNode *yaml.Node) bool {
-	isKeyNodeMarkedAsLocked := keyNode != nil && (lockMarkerRegex.MatchString(keyNode.HeadComment) ||
-		lockMarkerRegex.MatchString(keyNode.LineComment))
-	isValueNodeMarkedAsLocked := valueNode != nil &&
-		(valueNode.Kind == yaml.ScalarNode && lockMarkerRegex.MatchString(valueNode.LineComment))
-	return isKeyNodeMarkedAsLocked || isValueNodeMarkedAsLocked
+func IsLocked(key, value *yaml.Node) bool {
+	commentLocked := isCommentLocked(key) || isCommentLocked(value)
+	tagLocked := value != nil && value.Tag == "!lock"
+	return commentLocked || tagLocked
+}
+
+func isCommentLocked(node *yaml.Node) bool {
+	if node == nil {
+		return false
+	}
+	return lockMarkerRegex.MatchString(node.HeadComment) || lockMarkerRegex.MatchString(node.LineComment)
 }
