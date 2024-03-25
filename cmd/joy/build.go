nestoca/joy

commit 1d3c9fb4350cc5a18e33bc78e15ea59e02da57d5

feat(PL-2554): support chart refs
This reverts commit 52fbaa8da7cf584b010ea232b6356e8fa9d7e8ba.

cmd/joy/build.go
@@ -4,7 +4,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/nestoca/joy/internal/build"
-	"github.com/nestoca/joy/internal/config"
+	"github.com/nestoca/joy/pkg/catalog"
 )
 
 func NewBuildCmd() *cobra.Command {
@@ -33,10 +33,10 @@ Usage: joy build promote <env> <project> <version>`,
 			project := args[1]
 			version := args[2]
 
-			cfg := config.FromContext(cmd.Context())
+			cat := catalog.FromContext(cmd.Context())
 
 			return build.Promote(build.Opts{
-				CatalogDir:  cfg.CatalogDir,
+				Catalog:     cat,
 				Environment: env,
 				Project:     project,
 				Version:     version,

internal/build/promote.go
@@ -11,25 +11,16 @@ import (
 )
 
 type Opts struct {
-	CatalogDir  string
+	Catalog     *catalog.Catalog
+	Writer      yml.Writer
 	Environment string
 	Project     string
 	Version     string
 }
 
 func Promote(opts Opts) error {
-	// Load Catalog with one environment
-	loadOpts := catalog.LoadOpts{
-		Dir:      opts.CatalogDir,
-		EnvNames: []string{opts.Environment},
-	}
-	cat, err := catalog.Load(loadOpts)
-	if err != nil {
-		return fmt.Errorf("loading catalog: %w", err)
-	}
-
 	// Check the release version format
-	if !cat.Environments[0].Spec.Promotion.FromPullRequests {
+	if !opts.Catalog.Environments[0].Spec.Promotion.FromPullRequests {
 		version := "v" + opts.Version
 		if semver.Prerelease(version)+semver.Build(version) != "" {
 			return fmt.Errorf("cannot promote release with non-standard version to %s environment", opts.Environment)
@@ -37,27 +28,23 @@ func Promote(opts Opts) error {
 	}
 
 	promotionCount := 0
-	for _, crossRelease := range cat.Releases.Items {
+	for _, crossRelease := range opts.Catalog.Releases.Items {
 		release := crossRelease.Releases[0]
 		if release.Spec.Project == opts.Project {
-			// Find version node
 			versionNode, err := yml.FindNode(release.File.Tree, "spec.version")
 			if err != nil {
 				return fmt.Errorf("release %s has no version property: %w", release.Name, err)
 			}
 
-			// Update version node
 			versionNode.Value = opts.Version
-			err = release.File.UpdateYamlFromTree()
-			if err != nil {
+			if err := release.File.UpdateYamlFromTree(); err != nil {
 				return fmt.Errorf("updating release yaml from node tree: %w", err)
 			}
 
-			// Write release file back
-			err = release.File.WriteYaml()
-			if err != nil {
+			if err := opts.Writer.WriteFile(release.File); err != nil {
 				return fmt.Errorf("writing release file: %w", err)
 			}
+
 			fmt.Printf("✅ Promoted release %s to version %s\n", style.Resource(release.Name), style.Version(opts.Version))
 			promotionCount++
 		}

internal/release/cross/release_list.go
@@ -22,16 +22,16 @@ type ReleaseList struct {
 	Items        []*Release
 }
 
-// NewReleaseList creates a new ReleaseList
-func NewReleaseList(environments []*v1alpha1.Environment) *ReleaseList {
-	return &ReleaseList{
+// MakeReleaseList creates a new ReleaseList
+func MakeReleaseList(environments []*v1alpha1.Environment) ReleaseList {
+	return ReleaseList{
 		Environments: environments,
 	}
 }
 
 // LoadReleaseList loads all releases for given environments underneath the given base directory.
-func LoadReleaseList(allFiles []*yml.File, environments []*v1alpha1.Environment, projects []*v1alpha1.Project, releaseFilter filtering.Filter) (*ReleaseList, error) {
-	crossReleases := NewReleaseList(environments)
+func LoadReleaseList(allFiles []*yml.File, environments []*v1alpha1.Environment, projects []*v1alpha1.Project, releaseFilter filtering.Filter) (ReleaseList, error) {
+	crossReleases := MakeReleaseList(environments)
 	for _, file := range allFiles {
 		env := findEnvironmentForReleaseFile(environments, file)
 		if env == nil {
@@ -40,7 +40,7 @@ func LoadReleaseList(allFiles []*yml.File, environments []*v1alpha1.Environment,
 
 		rel, err := v1alpha1.LoadRelease(file)
 		if err != nil {
-			return nil, fmt.Errorf("loading release %s: %w", file.Path, err)
+			return ReleaseList{}, fmt.Errorf("loading release %s: %w", file.Path, err)
 		}
 
 		if rel.Spec.Project != "" && projects != nil && len(projects) != 0 {
@@ -58,7 +58,7 @@ func LoadReleaseList(allFiles []*yml.File, environments []*v1alpha1.Environment,
 		// Add release to cross-release list
 		err = crossReleases.addRelease(rel, env)
 		if err != nil {
-			return nil, fmt.Errorf("adding release %s to environment %s: %w", rel.Name, env.Name, err)
+			return ReleaseList{}, fmt.Errorf("adding release %s to environment %s: %w", rel.Name, env.Name, err)
 		}
 	}
 
@@ -134,10 +134,7 @@ func (r *ReleaseList) addRelease(rel *v1alpha1.Release, environment *v1alpha1.En
 }
 
 func (r *ReleaseList) SortedCrossReleases() []*Release {
-	var releases []*Release
-	for _, rel := range r.Items {
-		releases = append(releases, rel)
-	}
+	releases := append([]*Release{}, r.Items...)
 	sort.Slice(releases, func(i, j int) bool {
 		return releases[i].Name < releases[j].Name
 	})
@@ -145,8 +142,8 @@ func (r *ReleaseList) SortedCrossReleases() []*Release {
 }
 
 // OnlySpecificReleases returns a subset of the releases in this list that match the given names.
-func (r *ReleaseList) OnlySpecificReleases(releases []string) *ReleaseList {
-	subset := NewReleaseList(r.Environments)
+func (r *ReleaseList) OnlySpecificReleases(releases []string) ReleaseList {
+	subset := MakeReleaseList(r.Environments)
 	for _, item := range r.Items {
 		if slices.Contains(releases, item.Name) {
 			subset.Items = append(subset.Items, item)
@@ -157,10 +154,10 @@ func (r *ReleaseList) OnlySpecificReleases(releases []string) *ReleaseList {
 
 // GetReleasesForPromotion returns a subset of the releases in this list that are promotable,
 // with only the given source and target environments as first and second environments.
-func (r *ReleaseList) GetReleasesForPromotion(sourceEnv, targetEnv *v1alpha1.Environment) (*ReleaseList, error) {
+func (r *ReleaseList) GetReleasesForPromotion(sourceEnv, targetEnv *v1alpha1.Environment) (ReleaseList, error) {
 	sourceEnvIndex := r.getEnvironmentIndexByName(sourceEnv.Name)
 	targetEnvIndex := r.getEnvironmentIndexByName(targetEnv.Name)
-	subset := NewReleaseList([]*v1alpha1.Environment{sourceEnv, targetEnv})
+	subset := MakeReleaseList([]*v1alpha1.Environment{sourceEnv, targetEnv})
 	for _, item := range r.Items {
 		// Determine source and target releases
 		sourceRelease := item.Releases[sourceEnvIndex]
@@ -172,7 +169,7 @@ func (r *ReleaseList) GetReleasesForPromotion(sourceEnv, targetEnv *v1alpha1.Env
 		// Compute promoted file
 		err := newItem.ComputePromotedFile(sourceEnv, targetEnv)
 		if err != nil {
-			return nil, fmt.Errorf("computing promoted file for release %s: %w", item.Name, err)
+			return ReleaseList{}, fmt.Errorf("computing promoted file for release %s: %w", item.Name, err)
 		}
 		subset.Items = append(subset.Items, newItem)
 	}

api/v1alpha1/environment.go
@@ -7,6 +7,8 @@ import (
 
 	"gopkg.in/yaml.v3"
 
+	"github.com/davidmdm/x/xerr"
+
 	"github.com/nestoca/joy/internal/yml"
 )
 
@@ -42,6 +44,10 @@ type EnvironmentSpec struct {
 	// Namespace is the name of environment's namespace within cluster.
 	Namespace string `yaml:"namespace,omitempty" json:"namespace,omitempty"`
 
+	// ChartVersions allows the environment to override the given version of the catalog's chart references.
+	// This allows for environments to rollout new versions of chart references.
+	ChartVersions map[string]string `yaml:"chartVersions,omitempty" json:"chartVersions,omitempty"`
+
 	// Owners is the list of identifiers of owners of the environment.
 	// It can be any strings that uniquely identifies the owners, such as email addresses or Jac group identifiers.
 	Owners []string `yaml:"owners,omitempty" json:"owners,omitempty"`
@@ -69,6 +75,16 @@ type Environment struct {
 
 	// Dir is the path to the environment directory.
 	Dir string `yaml:"-" json:"-"`
+}
+
+func (env Environment) Validate(validChartRefs []string) error {
+	var errs []error
+	for ref := range env.Spec.ChartVersions {
+		if !slices.Contains(validChartRefs, ref) {
+			errs = append(errs, fmt.Errorf("unknown ref: %s", ref))
+		}
+	}
+	return xerr.MultiErrOrderedFrom("validating chart references", errs...)
 }
 
 func IsValidEnvironment(apiVersion, kind string) bool {

api/v1alpha1/release.go
@@ -23,14 +23,23 @@ type ReleaseMetadata struct {
 }
 
 type ReleaseChart struct {
-	// Name is the name of the chart.
-	Name string `yaml:"name,omitempty" json:"name,omitempty"`
-
-	// RepoUrl is the url of the chart repository.
+	Ref     string `yaml:"ref,omitempty"`
+	Version string `yaml:"version,omitempty"`
+	Name    string `yaml:"name,omitempty" json:"name,omitempty"`
 	RepoUrl string `yaml:"repoUrl,omitempty" json:"repoUrl,omitempty"`
+}
 
-	// Version of the chart.
-	Version string `yaml:"version,omitempty" json:"version,omitempty"`
+func (chart ReleaseChart) Validate(validRefs []string) error {
+	if (chart.RepoUrl == "") != (chart.Name == "") {
+		return fmt.Errorf("repoUrl and name must be defined together")
+	}
+	if chart.RepoUrl != "" && chart.Version == "" {
+		return fmt.Errorf("version is required when chart is not a reference")
+	}
+	if ref := chart.Ref; ref != "" && !slices.Contains(validRefs, ref) {
+		return fmt.Errorf("unknown ref: %s", ref)
+	}
+	return nil
 }
 
 type ReleaseSpec struct {

api/v1alpha1/release_test.go
@@ -29,3 +29,75 @@ func TestReleaseUnmarshalling(t *testing.T) {
 	require.Equal(t, nil, release.Spec.Values["nil"])
 	require.Equal(t, 42, release.Spec.Values["age"])
 }
+
+func TestReleaseChartValidation(t *testing.T) {
+	cases := []struct {
+		Name      string
+		Chart     v1alpha1.ReleaseChart
+		ValidRefs []string
+		Error     string
+	}{
+		{
+			Name: "only repoUrl",
+			Chart: v1alpha1.ReleaseChart{
+				RepoUrl: "test",
+			},
+			Error: "repoUrl and name must be defined together",
+		},
+		{
+			Name: "only name",
+			Chart: v1alpha1.ReleaseChart{
+				Name: "test",
+			},
+			Error: "repoUrl and name must be defined together",
+		},
+		{
+			Name: "repoUrl and name without version",
+			Chart: v1alpha1.ReleaseChart{
+				RepoUrl: "test",
+				Name:    "test",
+			},
+			Error: "version is required when chart is not a reference",
+		},
+		{
+			Name: "invalid ref",
+			Chart: v1alpha1.ReleaseChart{
+				Ref: "ref",
+			},
+			Error: "unknown ref: ref",
+		},
+		{
+			Name: "valid full chart def",
+			Chart: v1alpha1.ReleaseChart{
+				RepoUrl: "repo",
+				Name:    "name",
+				Version: "version",
+			},
+		},
+		{
+			Name: "valid ref without version",
+			Chart: v1alpha1.ReleaseChart{
+				Ref: "ref",
+			},
+			ValidRefs: []string{"ref"},
+		},
+		{
+			Name: "valid ref with version",
+			Chart: v1alpha1.ReleaseChart{
+				Ref:     "ref",
+				Version: "version",
+			},
+			ValidRefs: []string{"ref"},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			if tc.Error == "" {
+				require.NoError(t, tc.Chart.Validate(tc.ValidRefs))
+				return
+			}
+			require.EqualError(t, tc.Chart.Validate(tc.ValidRefs), tc.Error)
+		})
+	}
+}

cmd/joy/environment.go
@@ -43,7 +43,8 @@ Only selected environments will be included in releases table columns.`,
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cfg := config.FromContext(cmd.Context())
-			return environment.ConfigureSelection(cfg.CatalogDir, cfg.FilePath, allFlag)
+			cat := catalog.FromContext(cmd.Context())
+			return environment.ConfigureSelection(cat, cfg.FilePath, allFlag)
 		},
 	}
 	cmd.Flags().BoolVarP(&allFlag, "all", "a", false, "Select all environments")
@@ -58,7 +59,7 @@ func NewEnvironmentOpenCmd() *cobra.Command {
 		Args:    cobra.RangeArgs(0, 2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cfg := config.FromContext(cmd.Context())
-
+			cat := catalog.FromContext(cmd.Context())
 			envName := ""
 			if len(args) >= 1 {
 				envName = args[0]
@@ -67,14 +68,6 @@ func NewEnvironmentOpenCmd() *cobra.Command {
 			linkName := ""
 			if len(args) >= 2 {
 				linkName = args[1]
-			}
-
-			cat, err := catalog.Load(catalog.LoadOpts{
-				Dir:             cfg.CatalogDir,
-				SortEnvsByOrder: true,
-			})
-			if err != nil {
-				return fmt.Errorf("loading catalog: %w", err)
 			}
 
 			infoProvider := info.NewProvider(cfg.GitHubOrganization, cfg.Templates.Project.GitTag, cfg.RepositoriesDir, cfg.JoyCache)
@@ -105,6 +98,7 @@ func NewEnvironmentLinksCmd() *cobra.Command {
 		Args:    cobra.RangeArgs(0, 2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cfg := config.FromContext(cmd.Context())
+			cat := catalog.FromContext(cmd.Context())
 
 			envName := ""
 			if len(args) >= 1 {
@@ -114,14 +108,6 @@ func NewEnvironmentLinksCmd() *cobra.Command {
 			linkName := ""
 			if len(args) >= 2 {
 				linkName = args[1]
-			}
-
-			cat, err := catalog.Load(catalog.LoadOpts{
-				Dir:             cfg.CatalogDir,
-				SortEnvsByOrder: true,
-			})
-			if err != nil {
-				return fmt.Errorf("loading catalog: %w", err)
 			}
 
 			infoProvider := info.NewProvider(cfg.GitHubOrganization, cfg.Templates.Project.GitTag, cfg.RepositoriesDir, cfg.JoyCache)

cmd/joy/pr.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 
-	"github.com/nestoca/joy/internal/config"
 	"github.com/nestoca/joy/internal/pr/promote"
 	"github.com/nestoca/joy/pkg/catalog"
 )
@@ -28,17 +25,7 @@ func NewPRPromoteCmd() *cobra.Command {
 		Short:   "Auto-promote builds of pull request to given environment",
 		Args:    cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			cfg := config.FromContext(cmd.Context())
-
-			// Load catalog
-			loadOpts := catalog.LoadOpts{
-				Dir:             cfg.CatalogDir,
-				SortEnvsByOrder: true,
-			}
-			cat, err := catalog.Load(loadOpts)
-			if err != nil {
-				return fmt.Errorf("loading catalog: %w", err)
-			}
+			cat := catalog.FromContext(cmd.Context())
 
 			return promote.
 				NewDefaultPromotion(".").

commit 1bc0d0e33715d644606f4fd91ea57bd676616b1b

fix(PL-2582): fix build promote
1d3c9fb4350cc5a18e33bc78e15ea59e02da57d5 broke `build promote`
in a few ways:

- Did not have a writer
- Releases items now are in an array where the index is set per
environment. Not all environments may have a release.

cmd/joy/build.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/nestoca/joy/internal/build"
+	"github.com/nestoca/joy/internal/yml"
 	"github.com/nestoca/joy/pkg/catalog"
 )
 
@@ -40,6 +41,7 @@ Usage: joy build promote <env> <project> <version>`,
 				Environment: env,
 				Project:     project,
 				Version:     version,
+				Writer:      yml.DiskWriter,
 			})
 		},
 	}

internal/build/promote.go
@@ -28,8 +28,13 @@ func Promote(opts Opts) error {
 	}
 
 	promotionCount := 0
+	releaseIndex := opts.Catalog.Releases.GetEnvironmentIndexByName(opts.Environment)
 	for _, crossRelease := range opts.Catalog.Releases.Items {
-		release := crossRelease.Releases[0]
+		release := crossRelease.Releases[releaseIndex]
+		if release == nil {
+			// The release may not be present in the target environment
+			continue
+		}
 		if release.Spec.Project == opts.Project {
 			versionNode, err := yml.FindNode(release.File.Tree, "spec.version")
 			if err != nil {

internal/release/cross/release_list.go
@@ -81,8 +81,8 @@ func findEnvironmentForReleaseFile(environments []*v1alpha1.Environment, release
 	return nil
 }
 
-// getEnvironmentIndexByName returns the index of the environment with the given name or -1 if not found.
-func (r *ReleaseList) getEnvironmentIndexByName(name string) int {
+// GetEnvironmentIndexByName returns the index of the environment with the given name or -1 if not found.
+func (r *ReleaseList) GetEnvironmentIndexByName(name string) int {
 	for i, env := range r.Environments {
 		if env.Name == name {
 			return i
@@ -113,7 +113,7 @@ func (r *ReleaseList) getReleaseIndex(name string) int {
 // addRelease adds a release to given environment.
 func (r *ReleaseList) addRelease(rel *v1alpha1.Release, environment *v1alpha1.Environment) error {
 	// Find environment index
-	environmentIndex := r.getEnvironmentIndexByName(environment.Name)
+	environmentIndex := r.GetEnvironmentIndexByName(environment.Name)
 	if environmentIndex == -1 {
 		return fmt.Errorf("environment %s not found in list", environment.Name)
 	}
@@ -155,8 +155,8 @@ func (r *ReleaseList) OnlySpecificReleases(releases []string) ReleaseList {
 // GetReleasesForPromotion returns a subset of the releases in this list that are promotable,
 // with only the given source and target environments as first and second environments.
 func (r *ReleaseList) GetReleasesForPromotion(sourceEnv, targetEnv *v1alpha1.Environment) (ReleaseList, error) {
-	sourceEnvIndex := r.getEnvironmentIndexByName(sourceEnv.Name)
-	targetEnvIndex := r.getEnvironmentIndexByName(targetEnv.Name)
+	sourceEnvIndex := r.GetEnvironmentIndexByName(sourceEnv.Name)
+	targetEnvIndex := r.GetEnvironmentIndexByName(targetEnv.Name)
 	subset := MakeReleaseList([]*v1alpha1.Environment{sourceEnv, targetEnv})
 	for _, item := range r.Items {
 		// Determine source and target releases
@@ -184,7 +184,7 @@ func (r *ReleaseList) GetNonPromotableReleases(sourceEnv, targetEnv *v1alpha1.En
 	}
 
 	var invalidList []string
-	sourceEnvIndex := r.getEnvironmentIndexByName(sourceEnv.Name)
+	sourceEnvIndex := r.GetEnvironmentIndexByName(sourceEnv.Name)
 
 	for _, item := range r.Items {
 		sourceRelease := item.Releases[sourceEnvIndex]