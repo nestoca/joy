d2c6d4b51f1b010ffaf4042a694ec691121d0512
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2024-04-01 11:37:48 -0400

    fix(PL-2557): use moq for gitProvider mock

--- internal/release/promote/git_provider.go
+++ internal/release/promote/git_provider.go
@@ -1,6 +1,6 @@
-//go:generate mockgen -source=$GOFILE -destination=mock_$GOFILE -package=$GOPACKAGE
 package promote
 
+//go:generate moq -stub -out ./git_provider_mock.go . GitProvider
 type GitProvider interface {
 	CreateAndPushBranchWithFiles(branchName string, files []string, message string) error
 	CheckoutMasterBranch() error
