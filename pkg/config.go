7ed1b7bf1dffc13e572d2a20f94842907dc4f26b
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2024-05-01 09:00:39 -0400

    chore(PL-2662): export user and catalog config

--- pkg/config.go
+++ pkg/config.go
@@ -4,11 +4,15 @@ import (
 	"github.com/nestoca/joy/internal/config"
 )
 
-type Config = config.Config
+type (
+	Config        = config.Config
+	UserConfig    = config.User
+	CatalogConfig = config.Catalog
+)
 
-// LoadCatalogConfig takes the path to the catalog as input and loads any catalog specific
+// LoadConfigFromCatalog takes the path to the catalog as input and loads any catalog specific
 // configuration found in its .joyrc
-func LoadCatalogConfig(catalogPath string) (*Config, error) {
+func LoadConfigFromCatalog(catalogPath string) (*Config, error) {
 	var cfg Config
 	if err := config.LoadFile(catalogPath, &cfg.Catalog); err != nil {
 		return nil, err
