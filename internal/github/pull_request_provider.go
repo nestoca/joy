fa1cabdba5438f0c9e85f905f2e526254478a71b
Jean-Martin Archer <jean-martin.archer@nesto.ca>
2025-04-16 15:03:07 -0700

    fix(PL-3421): bots cannot be reviewers

--- internal/github/pull_request_provider.go
+++ internal/github/pull_request_provider.go
@@ -81,7 +81,7 @@ func (p *PullRequestProvider) Create(params pr.CreateParams) (string, error) {
 	}
 
 	for _, reviewer := range params.Reviewers {
-		if reviewer == "github-actions[bot]" {
+		if strings.HasSuffix(reviewer, "[bot]") {
 			continue
 		}
 		args = append(args, "--reviewer", reviewer)
