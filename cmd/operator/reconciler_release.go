d098e0c0e6755fe795e33367f059577640d4b8a1
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2026-06-11 16:28:18 -0400

    refactor(PL-6118): move argocd application into its own package

--- cmd/operator/reconciler_release.go
+++ cmd/operator/reconciler_release.go
@@ -8,16 +8,17 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	"github.com/nestoca/joy/api/v1alpha1"
+	"github.com/nestoca/joy/cmd/operator/argocd"
 )
 
 func ReleaseReconciler() ctrl.Funcs {
 	return ctrl.Funcs{
 		Handler: func(ctx context.Context, event ctrl.Event) (ctrl.Result, error) {
 			var (
-				client       = ctrl.Client(ctx)
 				releaseCache = ctrl.CacheFromEvent[v1alpha1.Release](ctx, event)
 				envCache     = ctrl.Cache[v1alpha1.Environment](ctx, schema.GroupKind{Group: "joy.nesto.ca", Kind: "Environment"}, "")
-				appIntf      = k8s.TypedInterface[Application](client.Dynamic, schema.GroupVersionResource{
+				client       = ctrl.Client(ctx)
+				appIntf      = k8s.TypedInterface[argocd.Application](client.Dynamic, schema.GroupVersionResource{
 					Group:    "argoproj.io",
 					Version:  "v1alpha1",
 					Resource: "applications",
