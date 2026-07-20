2d622daa6c19411cf258007e9c327784d5ae505d
David Desmarais-Michaud <desmarais.michaud.david@gmail.com>
2026-07-20 12:05:29 -0400

    fix(PL-6895): fix tests

--- internal/release/validate/validate_test.go
+++ internal/release/validate/validate_test.go
@@ -19,6 +19,12 @@ func TestValidateRelease(t *testing.T) {
 	disallowPullRequest := v1alpha1.Environment{Spec: v1alpha1.EnvironmentSpec{Promotion: v1alpha1.Promotion{FromPullRequests: false}}}
 	allowPullRequest := v1alpha1.Environment{Spec: v1alpha1.EnvironmentSpec{Promotion: v1alpha1.Promotion{FromPullRequests: true}}}
 
+	file := func(data string) *yml.File {
+		file, err := yml.NewFile("./test.yaml", []byte(data))
+		require.NoError(t, err)
+		return file
+	}
+
 	cases := []struct {
 		Name          string
 		Release       *v1alpha1.Release
@@ -28,8 +34,11 @@ func TestValidateRelease(t *testing.T) {
 		SkipReadCalls bool
 	}{
 		{
-			Name:    "spec matches schema",
-			Release: &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Values: map[string]any{"hello": "world"}}, Environment: &allowPullRequest},
+			Name: "spec matches schema",
+			Release: &v1alpha1.Release{
+				File:        file("spec: { values: { hello: world } }"),
+				Environment: &allowPullRequest,
+			},
 			ChartFS: &xfs.FSMock{
 				ReadFileFunc: func(string) ([]byte, error) { return []byte(`#values: { hello: string }`), nil },
 				DirNameFunc:  func() string { return "." },
@@ -37,24 +46,34 @@ func TestValidateRelease(t *testing.T) {
 			ExpectedErr: "",
 		},
 		{
-			Name:    "spec does not match schema",
-			Release: &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Values: map[string]any{"hello": true}}, Environment: &allowPullRequest},
+			Name: "spec does not match schema",
+			Release: &v1alpha1.Release{
+				File:        file("spec: { values: { hello: true } }"),
+				Environment: &allowPullRequest,
+			},
 			ChartFS: &xfs.FSMock{
 				ReadFileFunc: func(string) ([]byte, error) { return []byte(`#values: { hello: string }`), nil },
+				DirNameFunc:  func() string { return "." },
 			},
 			ExpectedErr: "hydrating values: unifying with chart schema: validating values: #values.hello: conflicting values string and true (mismatched types string and bool)",
 		},
 		{
-			Name:    "values missing from spec",
-			Release: &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Values: map[string]any{}}, Environment: &allowPullRequest},
+			Name: "values missing from spec",
+			Release: &v1alpha1.Release{
+				File:        file("spec: { values: { } }"),
+				Environment: &allowPullRequest,
+			},
 			ChartFS: &xfs.FSMock{
 				ReadFileFunc: func(string) ([]byte, error) { return []byte(`#values: { hello: string }`), nil },
 			},
 			ExpectedErr: "hydrating values: unifying with chart schema: validating values: #values.hello: incomplete value string",
 		},
 		{
-			Name:    "multiple errors",
-			Release: &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Values: map[string]any{"one": "one", "two": "two"}}, Environment: &allowPullRequest},
+			Name: "multiple errors",
+			Release: &v1alpha1.Release{
+				File:        file("spec: { values: { one: one, two: two } }"),
+				Environment: &allowPullRequest,
+			},
 			ChartFS: &xfs.FSMock{
 				ReadFileFunc: func(string) ([]byte, error) { return []byte(`#values: { one: 1, two: 2 }`), nil },
 			},
@@ -64,8 +83,11 @@ func TestValidateRelease(t *testing.T) {
 				"  - #values.two: conflicting values 2 and \"two\" (mismatched types int and string)",
 		},
 		{
-			Name:    "render fails",
-			Release: &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Values: map[string]any{"hello": "world"}}, Environment: &allowPullRequest},
+			Name: "render fails",
+			Release: &v1alpha1.Release{
+				File:        file("spec: { values: { hello: world } }"),
+				Environment: &allowPullRequest,
+			},
 			ChartFS: &xfs.FSMock{
 				ReadFileFunc: func(string) ([]byte, error) { return []byte(`#values: { hello: string }`), nil },
 				DirNameFunc:  func() string { return "" },
@@ -79,8 +101,11 @@ func TestValidateRelease(t *testing.T) {
 			ExpectedErr: "failed to render",
 		},
 		{
-			Name:    "no schema and render fails",
-			Release: &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Values: map[string]any{"hello": "world"}}, Environment: &allowPullRequest},
+			Name: "no schema and render fails",
+			Release: &v1alpha1.Release{
+				File:        file("spec: { values: { hello: world } }"),
+				Environment: &allowPullRequest,
+			},
 			ChartFS: &xfs.FSMock{
 				ReadFileFunc: func(string) ([]byte, error) { return nil, os.ErrNotExist },
 				DirNameFunc:  func() string { return "" },
@@ -93,16 +118,23 @@ func TestValidateRelease(t *testing.T) {
 			ExpectedErr: "failed to render",
 		},
 		{
-			Name:    "fail to read schema",
-			Release: &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Values: map[string]any{"hello": "world"}}, Environment: &allowPullRequest},
+			Name: "fail to read schema",
+			Release: &v1alpha1.Release{
+				File:        file("spec: { values: { hello: world } }"),
+				Environment: &allowPullRequest,
+			},
 			ChartFS: &xfs.FSMock{
 				ReadFileFunc: func(string) ([]byte, error) { return nil, errors.New("disk corrupted") },
 			},
 			ExpectedErr: "hydrating values: unifying with chart schema: reading values.cue: disk corrupted",
 		},
 		{
-			Name:    "standard version with disallow promotion from pull requests",
-			Release: &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Version: "1.0.0", Values: map[string]any{"hello": "world"}}, Environment: &disallowPullRequest, Project: &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{}}},
+			Name: "standard version with disallow promotion from pull requests",
+			Release: &v1alpha1.Release{
+				File:        file("spec: { version: 1.0.0, values: { hello: world } }"),
+				Environment: &disallowPullRequest,
+				Project:     &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{}},
+			},
 			ChartFS: &xfs.FSMock{
 				ReadFileFunc: func(string) ([]byte, error) { return []byte(`#values: { hello: string }`), nil },
 				DirNameFunc:  func() string { return "." },
@@ -110,14 +142,22 @@ func TestValidateRelease(t *testing.T) {
 			ExpectedErr: "",
 		},
 		{
-			Name:          "non-standard version with disallow promotion from pull requests",
-			Release:       &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Version: "1.0.0-rc.1+build.1"}, Environment: &disallowPullRequest, Project: &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{}}},
+			Name: "non-standard version with disallow promotion from pull requests",
+			Release: &v1alpha1.Release{
+				Spec:        v1alpha1.ReleaseSpec{Version: "1.0.0-rc.1+build.1"},
+				Environment: &disallowPullRequest,
+				Project:     &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{}},
+			},
 			SkipReadCalls: true,
 			ExpectedErr:   "invalid version: prerelease branches not allowed: 1.0.0-rc.1+build.1",
 		},
 		{
-			Name:    "non-standard version but skip pre-release check",
-			Release: &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Version: "2.133.0-pactbroker2.116.0", Values: map[string]any{"hello": "world"}}, Environment: &disallowPullRequest, Project: &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{SkipPreReleaseCheck: true}}},
+			Name: "non-standard version but skip pre-release check",
+			Release: &v1alpha1.Release{
+				File:        file("spec: { version: 2.133.0-pactbroker2.116.0, values: { hello: world } }"),
+				Environment: &disallowPullRequest,
+				Project:     &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{SkipPreReleaseCheck: true}},
+			},
 			ChartFS: &xfs.FSMock{
 				ReadFileFunc: func(string) ([]byte, error) { return []byte(`#values: { hello: string }`), nil },
 				DirNameFunc:  func() string { return "." },
@@ -125,19 +165,23 @@ func TestValidateRelease(t *testing.T) {
 			ExpectedErr: "",
 		},
 		{
-			Name:          "failing values hydration",
-			Release:       &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Values: map[string]any{"key": "$ref(.Invalid.Prefix.Ref)"}}, Environment: &v1alpha1.Environment{}, Project: &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{}}},
+			Name: "failing values hydration",
+			Release: &v1alpha1.Release{
+				File:        file("spec: { values: { key: '$ref(.Invalid.Prefix.Ref)' } }"),
+				Environment: &v1alpha1.Environment{},
+				Project:     &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{}},
+			},
 			ChartFS:       &xfs.FSMock{},
 			SkipReadCalls: true,
 			ExpectedErr:   `hydrating values: hydrating object values: only ".Environment.Spec.Values." prefix is supported for object interpolation, but found: .Invalid.Prefix.Ref`,
 		},
 		{
 			Name: "hydrated values matching schema",
-			Release: &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Values: map[string]any{"hello": "$ref(.Environment.Spec.Values.hello)"}}, Environment: &v1alpha1.Environment{
-				Spec: v1alpha1.EnvironmentSpec{
-					Values: map[string]any{"hello": "world"},
-				},
-			}, Project: &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{}}},
+			Release: &v1alpha1.Release{
+				File:        file("spec: { values: { hello: '$ref(.Environment.Spec.Values.hello)' } }"),
+				Environment: &v1alpha1.Environment{Spec: v1alpha1.EnvironmentSpec{Values: map[string]any{"hello": "world"}}},
+				Project:     &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{}},
+			},
 			ChartFS: &xfs.FSMock{
 				ReadFileFunc: func(string) ([]byte, error) { return []byte(`#values: { hello: "world" }`), nil },
 				DirNameFunc:  func() string { return "." },
@@ -146,11 +190,11 @@ func TestValidateRelease(t *testing.T) {
 		},
 		{
 			Name: "hydrated values not matching schema",
-			Release: &v1alpha1.Release{Spec: v1alpha1.ReleaseSpec{Values: map[string]any{"hello": "$ref(.Environment.Spec.Values.hello)"}}, Environment: &v1alpha1.Environment{
-				Spec: v1alpha1.EnvironmentSpec{
-					Values: map[string]any{"hello": "narnia"},
-				},
-			}, Project: &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{}}},
+			Release: &v1alpha1.Release{
+				File:        file("spec: { values: { hello: '$ref(.Environment.Spec.Values.hello)' } }"),
+				Environment: &v1alpha1.Environment{Spec: v1alpha1.EnvironmentSpec{Values: map[string]any{"hello": "narnia"}}},
+				Project:     &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{}},
+			},
 			ChartFS: &xfs.FSMock{
 				ReadFileFunc: func(string) ([]byte, error) { return []byte(`#values: { hello: "world" }`), nil },
 				DirNameFunc:  func() string { return "." },
@@ -162,11 +206,7 @@ func TestValidateRelease(t *testing.T) {
 			Release: &v1alpha1.Release{
 				Environment: &v1alpha1.Environment{},
 				Project:     &v1alpha1.Project{Spec: v1alpha1.ProjectSpec{}},
-				File: &yml.File{Tree: func() *yaml.Node {
-					var node yaml.Node
-					_ = yaml.Unmarshal([]byte(`{ value: !lock TODO }`), &node)
-					return &node
-				}()},
+				File:        file("{ value: !lock TODO }"),
 			},
 			ExpectedErr:   "contains locked TODO",
 			SkipReadCalls: true,
